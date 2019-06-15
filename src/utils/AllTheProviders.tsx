acf86ffa12df725dbbc27d5a69953421e928c867
Anthony Mittaz <sync@me.com>
2019-06-15 14:17:00 +1000

    Add Apollo Provider for tests

--- src/utils/AllTheProviders.tsx
+++ src/utils/AllTheProviders.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import RouterProvider from '../utils/RouterProvider';
+import ApolloProvider from './ApolloProvider';
 
 const AllTheProviders = ({ children }) => {
-  return <RouterProvider router={{ pathname: '/' }}>{children}</RouterProvider>;
+  return (
+    <RouterProvider router={{ pathname: '/' }}>
+      <ApolloProvider>{children}</ApolloProvider>
+    </RouterProvider>
+  );
 };
 
 export default AllTheProviders;
