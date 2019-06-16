f3d2c440ce1a2b92ec3828d75edfcda2846a19ef
Anthony Mittaz <sync@me.com>
2019-06-16 10:57:42 +1000

    Add mising ActiveLink stories

--- stories/ui.stories.tsx
+++ stories/ui.stories.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import Header from '../src/components/Header.gen';
+import ActiveLink from '../src/components/ActiveLink.gen';
 
 import './base.css';
 
-storiesOf('Header', module).add('Index', () => {
+storiesOf('Header', module).add('Default', () => {
   return <Header />;
 });
+
+storiesOf('ActiveLink', module)
+  .add('When Active', () => {
+    return (
+      <ActiveLink href="/" activeClassName={'test-active'}>
+        <span>{'Active Link'}</span>
+      </ActiveLink>
+    );
+  })
+  .add('When Inactive', () => {
+    return (
+      <ActiveLink href="/something" activeClassName={'test-active'}>
+        <span>{'Inactive Link'}</span>
+      </ActiveLink>
+    );
+  });
